d66883024c15134898044faa3cc7a0edebbcf365
AryanChouhan10 <92908961+AryanChouhan10@users.noreply.github.com>
2022-01-03 15:00:36 +0530

    Add files via upload

---
+++ FlightService/src/main/java/CaseStudyFlightService/FlightServiceApplication_1.java
@@ -0,0 +1,26 @@
+package CaseStudyFlightService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@EnableSwagger2
+@SpringBootApplication
+@EnableEurekaClient
+public class FlightServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightServiceApplication.class, args);
+	}
+	
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
